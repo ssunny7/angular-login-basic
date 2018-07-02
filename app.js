bd6c4f166007084d9f29d1dbf1880a4fb4dfbde9
Sunny Shah <shahsunny712@gmail.com>
2018-07-01 22:03:04 -0700

    Added comments explaining some components of the code

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 'use strict';
 
+/* These lines declare the function for use as a component of another module. */
 angular.module('Authentication', []);
 angular.module('Home', []);
 
+/* This defines a module while also specifying other modules as its
+   dependencies. In this case, the module name is also the name of the
+   HTML element in which the module will run. */
 angular.module('HttpAuthExample', [
     'Authentication',
     'Home',
+    // This is used to help make the page an SPA
     'ngRoute',
     'ngCookies'
 ])
+// $routeProvider provided by ngRoute
 .config(['$routeProvider', function($routeProvider) {
     $routeProvider
     .when('/login', {
@@ -21,6 +27,7 @@ angular.module('HttpAuthExample', [
     })
     .otherwise({redirectTo: '/login'});
 }])
+// Like the main() method in C/C++, executed first when angular app starts
 .run(['$rootScope', '$location', '$cookieStore', '$http',
     function($rootScope, $location, $cookieStore, $http) {
         $rootScope.globals = $cookieStore.get('globals') || {};
--- auth/controllers.js
+++ auth/controllers.js
@@ -1,7 +1,11 @@
 'use strict';
 
+/* A module is used to group services, controllers, directives, config etc that can be reused
+   in other modules. This is just the definition, it's declared for use in app.js. */
 angular.module('Authentication')
 .controller('LoginController',
+    /* The following line is dependency injection. The entries before 'function' are the
+       dependencies for this controller which are then accessed as parameters ib the function. */
     ['$scope', '$rootScope', '$location', 'AuthenticationService',
     function($scope, $rootScope, $location, AuthenticationService) {
         AuthenticationService.ClearCredentials();
